9b3c257b27b2d71d2d7e22387cb761df4f8486c1
MatW <mathieunlp@gmail.com>
2024-07-30 01:57:01 +0200

    dataloader + loss

--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,4 @@ def assign_check(left, right):
     if left.shape != right.shape:
         raise ValueError(f"Shape mismatch. Left: {left.shape}, Right: {right.shape}")
     return torch.nn.Parameter(right.clone().detach())
+
